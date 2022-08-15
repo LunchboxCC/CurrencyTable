53e4aa1ce5f56ec056dfc01a55cda314d896a769
LunchboxCC <lunchboxcc@gmail.com>
2022-08-15 12:27:31 +0200

    entity and context class created

---
+++ CurrencyTable/CurrencyTable/Database/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using CurrencyTable.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyTable.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        public virtual DbSet<Currency> Currencies { get; set; }
+
+        public ApplicationContext() { }
+
+        public ApplicationContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
